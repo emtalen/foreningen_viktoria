e16d4f39113823a7f912762e5e0f7f2a7799ce83
emtalen <59577366+emtalen@users.noreply.github.com>
2020-07-07 20:50:14 +0200

    adding a new css file for this component so i can keep the css files small, and making the bunnies a bit smaller

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import '../styles/startPage.css'
 
 const StartPage = () => {
   return (
-    <>
-      <img className="bunny" src="/images/bunny-right.png" alt="Bunny" />
-      <img className="bunny" src="/images/bunny-left.png" alt="Bunny" />
-    </>
+    <div className="bunny">
+      <img src="/images/bunny-right.png" alt="Bunny" />
+      <img src="/images/bunny-left.png" alt="Bunny" />
+    </div>
   );
 }
 
