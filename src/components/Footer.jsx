2741e1fbe224927f2ce6851590ac0b184418e409
emtalen <59577366+emtalen@users.noreply.github.com>
2020-07-08 14:36:37 +0200

    minor styling to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import '../styles/footer.css'
 
 const Footer = () => {
   return (
-    <>
-      <p>Föreningen Victoria</p>
+    <div id='footer'>
+      <div id='footer-info'><p>Föreningen Victoria</p>
       <p>Org nummer: 802529-3211</p>
       <p>Adress: Tallörtsgatan 13, 74563 Enköping</p>
-      <p>Kontakt: Arne Wåhlstedt 070-5890606</p>
-    </>
+      <p>Kontakt: Arne Wåhlstedt 070-5890606</p></div>
+      
+    </div>
   );
 }
 
