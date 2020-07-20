0c2e02d5ff041e33f0463c36778c0fbe2bc35157
emtalen <59577366+emtalen@users.noreply.github.com>
2020-07-20 11:20:39 +0200

    adding react-slideshow-image, the basic structure of if and the images that goes into the slide show

--- src/components/Galleri.jsx
+++ src/components/Galleri.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 import Navigation from "./Navigation";
+import { Slide } from "react-slideshow-image";
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+  onChange: (oldIndex, newIndex) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+  },
+};
 
 const Galleri = () => {
+  const contactInfo = [
+    // here goes some images
+  ];
   return (
     <div className="main-container">
       <div className="main-content">
         <Navigation />
         <div>
-          <h1>Galleri</h1>
+          <Slide {...properties}>
+            {contactInfo.map (info => {
+              return (
+                <div className='each-slide' key={info.id}>
+                  <div>
+                    <img src={info.img} alt={info.id}/>
+                  </div>
+                </div>
+              )
+            })}
+          </Slide>
         </div>
       </div>
     </div>
