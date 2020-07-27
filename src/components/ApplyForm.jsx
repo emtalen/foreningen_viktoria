a563ad38c46322a5d037e04f74ae311d9b54765f
emtalen <59577366+emtalen@users.noreply.github.com>
2020-07-27 14:06:15 +0200

    fixing so it is sending an email with emailjs and styling the form a bit

--- src/components/ApplyForm.jsx
+++ src/components/ApplyForm.jsx
@@ -1,21 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
 import "../styles/apply.css";
 
 const ApplyForm = () => {
+  const [message, setMessage] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "applicants_foreningen_victoria",
+        "applicants",
+        e.target,
+        "user_lpLITTsEfzW8hWs6z2ACY"
+      )
+      .then(
+        (result) => {
+          document.getElementById("applyform").reset();
+          setMessage(
+            "Tack för din ansökan. Vi återkopplar dig så snart som möjligt."
+          );
+        },
+        (error) => {
+          console.log(error.text);
+          setMessage(
+            "Tyvärr gick något snett, har du fyllt i alla fälten korrekt?"
+          );
+        }
+      );
+  };
   return (
-    <div className="main-container" id='apply'>
-      <div className='main-content'>
-        <span id='apply-header'>ANSÖK</span>
-        <form id="applyform" action="">
-          <label htmlFor="">Namn:</label>
-          <input type="text" />
-          <label htmlFor="">Mail:</label>
-          <input type="text" name="" id="" />
-          <label htmlFor="">Telefon:</label>
-          <input type="text" name="" id="" />
-          <label htmlFor="">Motivering:</label>
-          <input type="text" />
+    <div className="main-container" id="apply">
+      <div className="main-content">
+        <span id="apply-header">ANSÖK</span>
+        <br />
+        <form id="applyform" onSubmit={sendEmail}>
+          <input
+            placeholder="Namn:"
+            name="name"
+            id="name"
+            type="text"
+            required="true"
+          />
+          <br />
+          <input
+            placeholder="Email:"
+            type="email"
+            name="email"
+            id=""
+            required="true"
+          />
+          <br />
+          <input
+            placeholder="Telefonnummer:"
+            type="text"
+            name="phone"
+            id=""
+            required="true"
+          />
+          <br />
+          <input
+            placeholder="Motivering:"
+            id="motivation"
+            type="text"
+            name="message"
+            rows="6"
+            cols="40"
+            multiline
+            required="true"
+          />
+          <br />
+          <button id="submit-button" type="submit">
+            Skicka Ansökan
+          </button>
         </form>
+        <p id="message">{message}</p>
+        <p>Vid frågor, vänligen kontakta Arne Wåhlstedt</p>
       </div>
     </div>
   );
