39bf25df0639b2b34500ad1d1722a33ecb6ff979
emtalen <59577366+emtalen@users.noreply.github.com>
2020-08-06 10:51:09 +0200

    adding the swish and bg number to donations

--- src/components/Donations.jsx
+++ src/components/Donations.jsx
@@ -11,8 +11,12 @@ const Donations = () => {
       <div className="main-content">
         <span id="donations-header">DONERA</span>
         <p id="donations-text">
-          Vill du donera till Föreningen Victoria, vänligen ta kontakt med Arne
-          Wåhlstedt <br /> arne.enkoping@gmail.com <br /> 070-5890606
+          <b>Bankgiro:</b> 5493-6505 <br />
+          <b>Swish:</b> 123-536 74 46
+        </p>
+        <p id="donations-text">
+          Vid frågor angående donationer, vänligen ta kontakt med Arne Wåhlstedt{" "}
+          <br /> arne.enkoping@gmail.com <br /> 070-5890606
         </p>
         <button
           className="to-the-topp"
