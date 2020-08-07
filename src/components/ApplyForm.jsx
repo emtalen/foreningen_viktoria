62f793f79fc2a57541130d65575e52b86b5ef627
emtalen <59577366+emtalen@users.noreply.github.com>
2020-08-07 14:40:10 +0200

    will stop to make a fool out of myself very soon now

--- src/components/ApplyForm.jsx
+++ src/components/ApplyForm.jsx
@@ -11,10 +11,10 @@ const ApplyForm = () => {
     debugger
     emailjs
       .sendForm(
-        REACT_APP_SERVICE_ID,
-        REACT_APP_TEMPLATE_ID,
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
         e.target,
-        REACT_APP_USER_ID
+        process.env.REACT_APP_USER_ID
       )
       .then(
         (result) => {
