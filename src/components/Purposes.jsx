231b6096dd6c76ad5697a0f61900fb45d30809df
emtalen <59577366+emtalen@users.noreply.github.com>
2020-08-07 16:38:24 +0200

    fixing typos in the text

--- src/components/Purposes.jsx
+++ src/components/Purposes.jsx
@@ -4,27 +4,27 @@ import '../styles/purpose.css'
 
 const Purposes = () => {
   return (
-      <div className="main-container" id='purposes'>
-        <div className="main-content">
-          <Navigation />
-          <div id="purpose-text">
-            <p>
-              Föreningen har som ändåmål att bedriva ideell verksamhet i med
-              barn och ungdomar med särskilda behov / functionsnedsättning from
-              av vara utveckla deras fritidsintressen samt med särskild
-              målsättning att utvecklas till sitt bästa jag. Föreningen vänder
-              sig till människor i alla åldrar, oavsett bakgrund. Föreningen ska
-              aktivt arbeta för att motverka all form av diskriminering.
-            </p>
-            <p>
-              Utdelning <br />
-              Utdelning sker varje år till material, hjälpmedel, aktivitet eller
-              liknane som kanstödja den ansökande personen eller närstående,
-              beslut fattas av föreningens Styrelse.
-            </p>
-          </div>
+    <div className="main-container" id="purposes">
+      <div className="main-content">
+        <Navigation />
+        <div id="purpose-text">
+          <p>
+            Föreningen har som ändåmål att bedriva ideell verksamhet för barn
+            och ungdomar med särskilda behov / funktionsnedsättning i form av att
+            utveckla deras fritidsintressen samt med särskild målsättning att
+            utvecklas till sitt bästa jag. Föreningen vänder sig till människor
+            i alla åldrar, oavsett bakgrund. Föreningen ska arbetar aktivt för att motverka all form av
+            diskriminering.
+          </p>
+          <p>
+            Utdelning <br />
+            Utdelning sker varje år till material, hjälpmedel, aktivitet eller
+            liknane som kan mellanslag här stödja den ansökande personen eller
+            närstående. Beslutet fattas av föreningens Styrelse.
+          </p>
         </div>
       </div>
+    </div>
   );
 };
 
