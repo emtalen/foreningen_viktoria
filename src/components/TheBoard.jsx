7a2bb77d4682ed2508c040147130e5c703749bec
emtalen <59577366+emtalen@users.noreply.github.com>
2020-08-14 06:54:48 +0200

    fixing typos

--- src/components/TheBoard.jsx
+++ src/components/TheBoard.jsx
@@ -8,7 +8,7 @@ const TheBoard = () => {
       <div className="main-content">
         <Navigation />
         <h4>Ordförande: Arne Wåhlstedt 070-5890606</h4>
-        <h4>Sekreterare: Anders Eriksson 070-4539686</h4>
+        <h4>Sekreterare: Anders Eriksson 070-4539586</h4>
         <h4>Kassör: Ove Rickardsson 070-3075040</h4>
       </div>
     </div>
